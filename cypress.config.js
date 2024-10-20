4250dcbf8f50c9d4df37f4ed689676e8b7a5fd35
Mateusz <konatkowskimateusz@gmail.com>
2024-10-20 13:46:02 +0200

    testuuj

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+
+    },
+    excludeSpecPattern: ['**//cypress/e2e/1-getting-started','**//cypress/e2e/2-advanced-examples'],
+    baseUrl: 'https://automationexercise.com'
+  },
+});
